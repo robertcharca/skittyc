fc59c024a1db20bed7b86f7b488462c653ae46f4
Robert Charca <trevoraobac@gmail.com>
2023-05-16 13:21:19 -0500

    kfeatures: function for changing font values implemented

--- kittyc/kfeatures/kitty_fonts.go
+++ kittyc/kfeatures/kitty_fonts.go
@@ -1,10 +1,21 @@
 package kfeatures
 
+import (
+	"strings"
+
+	"github.com/robertcharca/skittyc/kittyc"
+)
+
 func SetNewFont () {
 	//
 }
 
-func ChangingFontValues () {
-	//	
+func ChangingFontValues (attribute, value string) {
+	fontAttribute := strings.ReplaceAll(attribute, " ", "_")
+
+	var fontValue string
+	fontValue = fontAttribute + " " + value
+
+	kittyc.WritingAtLine("# Fonts", fontValue)	
 }
 
