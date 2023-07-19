108994a22162dd19cafd3da6b5031e7d721cffff
Robert Charca <trevoraobac@gmail.com>
2023-07-19 13:40:27 -0500

    kittyc: 'searching similar values' function added

--- kittyc/searching.go
+++ kittyc/searching.go
@@ -4,12 +4,21 @@ import "errors"
 
 var notFound = errors.New("Value not found in array")
 
-func SearchingValue (list []string, value string) (bool, error) {
+func SearchingValue(list []string, value string) (bool, error) {
 	for _, sv := range list {
 		if sv == value {
 			return true, nil
 		} 
 	}
 
+	return false, notFound
+}
+
+func SearchingSimilarValues(list []string, svalue string) (bool, error) {	
+	for l := 0; l <= len(list); l++ {
+		if list[l][:4] == svalue[:4] {
+			return true, nil
+		}
+	}
 	return false, notFound
 }
